ac90553e77d3ed8706f944d8eca9643bf58c6be8
Gaspare <Gas9220@users.noreply.github.com>
2023-03-22 17:16:11 +0100

    Spostato il titolo in un componente

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script>
+import AppTitle from './components/AppTitle.vue';
+
 export default {
-  data() {
-    return {
-      message: "La mia prima app con Vite!"
-    }
+  components: {
+    AppTitle
   }
 }
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <AppTitle></AppTitle>
 </template>
 
 <style scoped>
